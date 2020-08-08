2b5b68f4d5f8d506ea8ac5312da15c5b0dcd3187
chkern <c.kern@uni-mannheim.de>
2020-08-08 18:33:03 +0200

    Start doc with roxygen2

---
+++ R/kwml.R
@@ -0,0 +1,12 @@
+#' KWML: Boosted Kernel Weighting for R
+#'
+#' R package for "Boosted Kernel Weighting - Using Statistical Learning to Improve Inference from Nonprobability Samples"
+#'
+#' @importFrom survey svydesign svyglm
+#' @importFrom cobalt bal.tab
+#' @importFrom partykit glmtree cforest
+#' @importFrom gbm gbm
+#'
+#' @docType package
+#' @name KWML
+NULL
